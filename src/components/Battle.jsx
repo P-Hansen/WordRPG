27b10cf454a65c62546c1a66559be20518648875
P-Hansen <philip.pt.hansen@gmail.com>
2021-06-24 20:18:47 -0600

    basic menu box positioning

--- src/components/Battle.jsx
+++ src/components/Battle.jsx
@@ -1,13 +1,20 @@
 import "./Battle.scss";
 
 function Battle() {
+
+    const attacks = ["spell 1", "spell 2", "spell 3"];
+
     return (
         <div className="background" style={{
             backgroundImage: 'url(/paper.jpg)',
             backgroundRepeat: 'no-repeat',
             backgroundSize: '100% 100%'
             }} >
-            <div className="menuBox"></div>
+            <div className="menuBox">
+                {attacks[0]}<br/>
+                {attacks[1]}<br/>
+                {attacks[2]}
+            </div>
         </div>
     )
 }
