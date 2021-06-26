1046cd86c2ff544c92b0a5c9612e11ce600e5a0c
P-Hansen <philip.pt.hansen@gmail.com>
2021-06-25 22:12:23 -0600

    new char sprites

--- src/components/Battle.jsx
+++ src/components/Battle.jsx
@@ -2,8 +2,28 @@ import "./Battle.scss";
 
 function Battle() {
 
-    let Mage = {
+    let Mage1 = {
         image: "Lanto47.png",
+        hp: 100,
+        armor: 2,
+        dodge: 10,
+        speed: 5,
+        resistance: 5,
+        attacks: ["spell 1", "spell 2", "spell 3"],
+    }
+
+    let Mage2 = {
+        image: "Lanto138.png",
+        hp: 100,
+        armor: 2,
+        dodge: 10,
+        speed: 5,
+        resistance: 5,
+        attacks: ["spell 1", "spell 2", "spell 3"],
+    }
+
+    let Mage3 = {
+        image: "Lanto113.png",
         hp: 100,
         armor: 2,
         dodge: 10,
@@ -43,19 +63,19 @@ function Battle() {
             <img className="characterImage1" style={{
                 width: '33px',
                 height: '33px',
-                background: `url(${Mage.image}) 0 -64px`,
+                background: `url(${Mage1.image}) 0 -64px`,
                 zIndex: '1'
                 }} />
             <img className="characterImage2" style={{
                 width: '33px',
                 height: '33px',
-                background: `url(${Mage.image}) 0 -64px`,
+                background: `url(${Mage2.image}) 0 -64px`,
                 zIndex: '1'
                 }} />
             <img className="characterImage3" style={{
                 width: '33px',
                 height: '33px',
-                background: `url(${Mage.image}) 0 -64px`,
+                background: `url(${Mage3.image}) 0 -64px`,
                 zIndex: '1'
                 }} />
             <img className="enemy1" src={Enemy1.image} style={{
@@ -69,9 +89,9 @@ function Battle() {
                 zIndex: '1'
                 }} />
             <div className="menuBox">
-                {Mage.attacks[0]}<br/>
-                {Mage.attacks[1]}<br/>
-                {Mage.attacks[2]}
+                {Mage1.attacks[0]}<br/>
+                {Mage1.attacks[1]}<br/>
+                {Mage1.attacks[2]}
             </div>
         </div>
         </>
