9c985c2a636b8d863d844d36f1d16f8431536b3d
P-Hansen <philip.pt.hansen@gmail.com>
2021-06-25 20:16:38 -0600

    player character object

--- src/components/Battle.jsx
+++ src/components/Battle.jsx
@@ -2,20 +2,37 @@ import "./Battle.scss";
 
 function Battle() {
 
-    const attacks = ["spell 1", "spell 2", "spell 3"];
+    let Mage = {
+        image: "/$Lanto (47).png",
+        hp: 100,
+        armor: 2,
+        dodge: 10,
+        speed: 5,
+        resistance: 5,
+        attacks: ["spell 1", "spell 2", "spell 3"],
+    }
 
     return (
+        <>
         <div className="background" style={{
             backgroundImage: 'url(/paper.jpg)',
             backgroundRepeat: 'no-repeat',
-            backgroundSize: '100% 100%'
-            }} >
+            backgroundSize: '100% 100%',
+            zIndex: '-10'
+        }} >
+            <img className="characterImage" style={{
+                width: '32px',
+                height: '42px',
+                background: `url(${Mage.image}) 0 0`,
+                zIndex: '1'
+                }} />
             <div className="menuBox">
-                {attacks[0]}<br/>
-                {attacks[1]}<br/>
-                {attacks[2]}
+                {Mage.attacks[0]}<br/>
+                {Mage.attacks[1]}<br/>
+                {Mage.attacks[2]}
             </div>
         </div>
+        </>
     )
 }
 
