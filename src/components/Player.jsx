b84625069cf6e3fa7183f23394f3f77a37ddfc96
Ted <ted@ted-ubuntu-desktop>
2021-06-28 20:53:35 -0600

    add use-key-press.js hook

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,26 @@
 import Actor from "./Actor";
+import useKeyPress from "../hooks/use-key-press";
 
+// this is the component that listens for player input
 const Player = ({ skin }) => {
 
   const data = {
     h: 32,
     w: 32,
-  }
+  };
+
+  const directions = {
+    down: 0,
+    left: 1,
+    right: 2,
+    up: 3,
+  };
+
+  useKeyPress((event) => {
+    event.preventDefault();
+    const dir = event.key.replace("Arrow", "").toLowerCase();
+    console.log(dir);
+  });
   
   return (
     <Actor sprite={`/sprites/skins/${skin}.png`} data={data}/>
