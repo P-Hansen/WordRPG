887f54b85558889eb5c7cebfed3475ac8a89f2dd
Ted <ted@ted-ubuntu-desktop>
2021-06-28 20:30:23 -0600

    add prop handling to Actor Component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import Battle from './components/Battle.jsx';
 import Actor from "./components/Actor";
 
 function App() {
+
+  const data = {
+    h: 32,
+    w: 32,
+  }
+
   return (
     <>
       <div className="zone-container">
-       <Actor />
+       <Actor sprite={`/sprites/skins/m2.png`} data={data}/>
       </div>
       <div>
         <Battle />
