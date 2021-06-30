ee29b164cdc0f1014cef5a8dc23fd547209d9eb1
P-Hansen <philip.pt.hansen@gmail.com>
2021-06-29 22:02:45 -0600

    world/battle state

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import Player from "./components/Player";
 
 function App() {
 
+  let [state, setState] = useState("WORLD");
+
   return (
     <>
       <div className="zone-container">
+      {state === "WORLD" && (
        <Player skin="m2"/>
+      )}
       </div>
       <div>
+      {state === "BATTLE" && (
         <Battle />
+      )}
       </div>
     </>
   );
