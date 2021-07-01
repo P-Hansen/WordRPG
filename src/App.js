d0967aa1133f91b785678de3202a538bdb7fe63d
P-Hansen <philip.pt.hansen@gmail.com>
2021-06-30 22:10:59 -0600

    moved characters to a new file

--- src/App.js
+++ src/App.js
@@ -2,40 +2,11 @@ import './App.scss';
 import { useState } from "react";
 import Battle from './components/Battle.jsx';
 import Player from "./components/Player";
+import characters from './helpers/characters';
 
 function App() {
 
   let [state, setState] = useState("BATTLE");
-
-  let Mage1 = {
-    image: "Lanto47.png",
-    hp: 100,
-    armor: 2,
-    dodge: 10,
-    speed: 5,
-    resistance: 5,
-    attacks: [{name:"spell 1", description: "description here", dmg: 100}, {name:"spell 2", description: "description here", dmg: 200}, {name:"spell 3", description: "description here", dmg: 50}],
-  }
-
-  let Mage2 = {
-      image: "Lanto138.png",
-      hp: 100,
-      armor: 2,
-      dodge: 10,
-      speed: 5,
-      resistance: 5,
-      attacks: [{name:"spell 4", description: "description here", dmg: 100}, {name:"spell 5", description: "description here", dmg: 200}, {name:"spell 6", description: "description here", dmg: 50}],
-  }
-
-  let Mage3 = {
-      image: "Lanto113.png",
-      hp: 100,
-      armor: 2,
-      dodge: 10,
-      speed: 5,
-      resistance: 5,
-      attacks: [{name:"spell 7", description: "description here", dmg: 100}, {name:"spell 8", description: "description here", dmg: 200}, {name:"spell 9", description: "description here", dmg: 50}],
-  }
 
   return (
     <>
@@ -46,7 +17,7 @@ function App() {
       </div>
       <div>
       {state === "BATTLE" && (
-        <Battle characters={[Mage1, Mage2, Mage3]} />
+        <Battle characters={characters} />
       )}
       </div>
     </>
--- src/helpers/characters.js
+++ src/helpers/characters.js
@@ -1,30 +1,38 @@
-
-let Mage1 = {
-    image: "Lanto47.png",
+export default [
+    {image: "Lanto47.png",
     hp: 100,
     armor: 2,
     dodge: 10,
     speed: 5,
     resistance: 5,
-    attacks: [{name:"spell 1", description: "description here", dmg: 100}, {name:"spell 2", description: "description here", dmg: 200}, {name:"spell 3", description: "description here", dmg: 50}],
-}
-
-let Mage2 = {
-    image: "Lanto138.png",
+    attacks: [
+      {name:"spell 1", description: "description here", dmg: 100},
+      {name:"spell 2", description: "description here", dmg: 200},
+      {name:"spell 3", description: "description here", dmg: 50}
+    ]
+    },
+    {image: "Lanto138.png",
     hp: 100,
     armor: 2,
     dodge: 10,
     speed: 5,
     resistance: 5,
-    attacks: ["spell 1", "spell 2", "spell 3"],
-}
-
-let Mage3 = {
-    image: "Lanto113.png",
+    attacks: [
+        {name:"spell 4", description: "description here", dmg: 100},
+        {name:"spell 5", description: "description here", dmg: 200},
+        {name:"spell 6", description: "description here", dmg: 50}
+    ],
+    },
+    {image: "Lanto113.png",
     hp: 100,
     armor: 2,
     dodge: 10,
     speed: 5,
     resistance: 5,
-    attacks: ["spell 1", "spell 2", "spell 3"],
-}
+    attacks: [
+        {name:"spell 7", description: "description here", dmg: 100},
+        {name:"spell 8", description: "description here", dmg: 200},
+        {name:"spell 9", description: "description here", dmg: 50}
+    ],
+    }
+]
