397e3743eb1bcc90c5e9aebdbdba372829620183
P-Hansen <philip.pt.hansen@gmail.com>
2021-07-03 12:42:38 -0600

    enemies randomly choose one of their attacks and use them

--- src/helpers/enemyTurn.js
+++ src/helpers/enemyTurn.js
@@ -1,5 +1,8 @@
-export default function enemyTurn(enemyArray, playerArray) {
+function enemyTurn(enemyArray, playerArray) {
     enemyArray.map((enemy)=>{
-        
-    })
-}
+        let target = playerArray[Math.floor(Math.random()*playerArray.length)];
+        target.hp -= enemy.attacks[Math.floor(Math.random()*playerArray.length)].dmg;
+    });
+};
+
+export default enemyTurn;
