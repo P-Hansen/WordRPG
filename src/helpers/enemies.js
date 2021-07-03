56e85ce8d3ce09bfaedde592212f63f7b76c5f19
P-Hansen <philip.pt.hansen@gmail.com>
2021-07-03 12:48:01 -0600

    enemy array length bug

--- src/helpers/enemies.js
+++ src/helpers/enemies.js
@@ -8,7 +8,7 @@ export default [
         dodge: 10,
         speed: 5,
         resistance: 5,
-        attacks: [{name: "slash", dmg: 20}, {name: "bite", dmg: 50}],
+        attacks: [{name: "slash", dmg: 40}, {name: "bite", dmg: 80}],
     },
     {
         name: "Oviraptor",
@@ -18,5 +18,5 @@ export default [
         dodge: 15,
         speed: 6,
         resistance: 3,
-        attacks: [{name: "slash", dmg: 20}, {name: "bite", dmg: 50}],
+        attacks: [{name: "slash", dmg: 40}, {name: "bite", dmg: 80}],
     }];
--- src/helpers/enemyTurn.js
+++ src/helpers/enemyTurn.js
@@ -1,7 +1,7 @@
 function enemyTurn(enemyArray, playerArray) {
     enemyArray.map((enemy)=>{
         let target = playerArray[Math.floor(Math.random()*playerArray.length)];
-        target.hp -= enemy.attacks[Math.floor(Math.random()*playerArray.length)].dmg;
+        target.hp -= enemy.attacks[Math.floor(Math.random()*enemy.attacks.length)].dmg;
     });
 };
 
