e7c9728c9f81ac7ea0c35d7a31a9d7b247e2f4e5
P-Hansen <philip.pt.hansen@gmail.com>
2021-07-06 17:25:08 -0600

    prevent overheal on health bar

--- src/components/HealthBar.jsx
+++ src/components/HealthBar.jsx
@@ -1,9 +1,14 @@
 import "./HealthBar.scss";
 //draws a rectangle for current Hp and a boarder for max Hp most of the work is done in the sass file
 function HealthBar(props) {
+
+    function max(num, max) {
+        return (num > max) ? max : num;
+    }
+
     return(
         <>
-        <div className="bar" style={{ width: props.currentHp }}/>
+        <div className="bar" style={{ width: max(props.currentHp, props.maxHp) }}/>
         <div className="barBack" style={{ width: props.maxHp-1 }}/>
         </>
     )
