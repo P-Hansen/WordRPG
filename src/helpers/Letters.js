812ec54278b3003b32d43408752fc32def85647b
P-Hansen <philip.pt.hansen@gmail.com>
2021-07-13 14:26:56 -0600

    comments

--- src/helpers/Letters.js
+++ src/helpers/Letters.js
@@ -1,18 +1,22 @@
 export default {
-    letters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    //string or "alphabet" of currently available letters
+    letters: "ABCDEFGHIJKLMNOPQRSTUVWXYZAEIOUABCDEFGHIJKLMNOPQRSTUVWXYZAEIOUAEIOU",
     howMany(letter) {
         const count = this.letters.match(new RegExp(letter, 'g'));
         return count ? count.length : 0;
     },
+    //adds the given letters to the alphabet
     add(letter) {
         this.letters += letter;
     },
+    //removes all the letters of the given word from the alphabet
     subtract(word){
         let wordArray = word.split('');
         wordArray.map((letter)=>{
             this.letters = this.letters.replace(letter, '');
         });
     },
+    //takes in a word and returns the letters styled grey if they are missing
     wordColour(word){
         let wordArray = word.split('');
         return wordArray.map((letter)=>{
